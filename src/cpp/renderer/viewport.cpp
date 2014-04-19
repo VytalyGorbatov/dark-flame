fad5a9fbd084a32aee8ce3af0fabbf77c86ceaad
Inverse <inverse@ua.fm>
2014-04-19 22:03:17 +0300

    Add VIEWPORT class.

---
+++ src/cpp/renderer/viewport.cpp
@@ -0,0 +1,17 @@
+#include "renderer.hpp"
+#include "viewport.hpp"
+
+using namespace renderer;
+
+VIEWPORT::VIEWPORT(int x, int y, int w, int h)
+{
+    this->x = x > 0 ? x : 0;
+    this->y = y > 0 ? y : 0;
+    this->w = w > 0 ? w : 0;
+    this->h = h > 0 ? h : 0;
+}
+
+void VIEWPORT::apply() const
+{
+    glViewport(x, y, w, h);
+}
---
+++ src/cpp/renderer/viewport.hpp
@@ -0,0 +1,23 @@
+#ifndef __VIEWPORT_HPP__
+#define __VIEWPORT_HPP__
+
+namespace renderer
+{
+
+class VIEWPORT
+{
+private:
+    int x;
+    int y;
+    int w;
+    int h;
+
+public:
+    VIEWPORT(int x_left_bot, int y_left_bot, int width, int height);
+
+    void apply() const;
+};
+
+} // namespace renderer
+
+#endif  // __VIEWPORT_HPP__
