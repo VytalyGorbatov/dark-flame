9090f7ed72c4e79bb085de34a7f23157db35c493
Inverse <inverse@ua.fm>
2014-11-03 21:43:27 +0200

    Add basic OGL settings.

--- src/cpp/renderer/renderer.hpp
+++ src/cpp/renderer/renderer.hpp
@@ -29,7 +29,11 @@ namespace renderer
 #include "ogl.hpp"
 
 /* Later will be replaced with abstract class for several renderers. */
-class RENDERER;
+class RENDERER
+{
+public:
+    static void init();
+};
 
 }
 
