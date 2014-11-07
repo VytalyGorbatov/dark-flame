ec09f511d2344c1ba006d4650bc36295319133f9
Inverse <inverse@ua.fm>
2014-11-07 19:31:56 +0200

    Add physics object base class.

---
+++ src/cpp/physic/object.hpp
@@ -0,0 +1,36 @@
+/**
+ * DarkFlame Copyright (C) 2014 Alexey Shumeiko
+ *
+ * This file is part of DarkFlame.
+ *
+ * DarkFlame is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * DarkFlame is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with DarkFlame.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * For more details see LICENSE file.
+ */
+
+#ifndef __OBJECT_HPP__
+#define __OBJECT_HPP__
+
+#include "vector.hpp"
+
+namespace physic
+{
+
+class OBJECT
+{
+};
+
+} // namespace physic
+
+#endif // __OBJECT_HPP__
